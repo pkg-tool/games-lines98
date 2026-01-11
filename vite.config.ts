edb3de8b93fde6f48bd43f6d3d2ac8d022a424de
c823g <c823g@example.com>
2026-01-11 11:57:54 +0100

    ⟦◎▪◎⟧

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   base: './', // Use relative paths for assets to work on GitHub Pages and local file preview
+  build: {
+    rollupOptions: {
+      input: 'index.dev.html',
+    },
+  },
+  server: {
+    open: '/index.dev.html',
+  },
 })
